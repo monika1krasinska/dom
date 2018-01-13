2016a7381adaf094ce13b714e0ee496cabf50ed4
Monika Krasińska <monika.krasinska@o2.pl>
2018-01-13 10:24:23 +0100

    commit -m"znajdywanie-elementow"

---
+++ js/2-znajdywanie-elementow.js
@@ -0,0 +1,11 @@
+'use strict';
+//var parFirst = document.getElementById("parFirst");
+//console.log(parFirst);
+//var link = document.getElementsByClassName("superlink")
+//console.log(link)
+//var linkiPoTagu = document.getElementsByTagName("a");
+//console.log(linkiPoTagu);
+//var divPoId = document.querySelector("#parSecond")
+//console.log(divPoId)
+var selectorLink = document.querySelectorAll(".superlink")
+console.log(selectorLink)
